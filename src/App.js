3d3c29c86b894cd172b44e129c4a1f9ca7da151a
Tigran-React-Dev <hambardzumyant351@gmail.com>
2022-09-04 10:34:47 +0400

    add Header component and make sketch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import FilterBar from "./components/FilterBar"
+
 function App() {
 
-   console.log("app");
+  
 
   return (
     <div className="App">
-         <Header/>
+     <Header/>
          
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import css from "../assets/styles/Header.module.scss";
-
-
+import logo from "../assets/image/img/logo.svg"
+import avatar from "../assets/image/icon/user_avatar.png"
 
 const Header =()=>{
 
 
     return (
-         <div>
-           
+         <div className={css.header}>
+              <img 
+                  src={logo} 
+                  alt="logo" 
+                  className={css.logo} 
+                  onClick={()=>{
+                    window.scrollTo(0,0)
+                  }}
+                  />
+               <img src={avatar} alt="user avatar" className={css.avatar} />   
+
          </div>
     )
 }
